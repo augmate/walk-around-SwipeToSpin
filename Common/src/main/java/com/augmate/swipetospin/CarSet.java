6a9f60b39a4a1f65059fcee90971632c136f5abc
frank <frank@augmate.com>
2014-10-17 17:58:39 -0400

    added container for sets of car images. added car sample images

---
+++ Common/src/main/java/com/augmate/swipetospin/CarSet.java
@@ -0,0 +1,19 @@
+package com.augmate.swipetospin;
+
+import org.opencv.core.Mat;
+
+import java.util.ArrayList;
+
+/**
+ * Created by frank0631 on 10/17/14.
+ */
+public class CarSet {
+
+    public ArrayList<Mat> exterior;
+    public int exteriorNum;
+
+    public void loadFromDir(String dir){
+
+    }
+
+}
